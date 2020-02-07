966dcf0011e5fd1da566e597fc6895c65bc81611
annahung31 <fbiannahung@gmail.com>
2020-02-07 11:14:29 +0800

    change username

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
 sh Anaconda3-2019.07-Linux-x86_64.sh -y
 ssh-keygen -t rsa -b 4096 -C fbiannahung@gmail.com
 git config --global user.email fbiannahung@gmail.com
-git config --global user.name annahung
+git config --global user.name annahung31
 
 echo "to use git, please"
 echo "go to ~/.ssh/id_rsa.pub to copy the SSH key, and paste it on gitlab->profile->SSH key"
