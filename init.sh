9a16981c9f9fbe964baaebbb925d53384e14ad8a
annahung31 <fbiannahung@gmail.com>
2020-02-07 11:13:17 +0800

    first version

---
+++ init.sh
@@ -0,0 +1,15 @@
+apt-get update
+apt-get install git
+apt-get install wget -y
+apt-get install ffmpeg -y
+apt-get install vim -y
+apt-get install tmux -y
+apt-get install zip
+wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+sh Anaconda3-2019.07-Linux-x86_64.sh -y
+ssh-keygen -t rsa -b 4096 -C fbiannahung@gmail.com
+git config --global user.email fbiannahung@gmail.com
+git config --global user.name annahung
+
+echo "to use git, please"
+echo "go to ~/.ssh/id_rsa.pub to copy the SSH key, and paste it on gitlab->profile->SSH key"
---
+++ toolkits.sh
@@ -0,0 +1,10 @@
+
+pip install numpy==1.16.4
+pip install argparse
+apt-get update
+conda install pytorch==1.2.0 torchvision==0.4.0 cudatoolkit=10.0 -c pytorch
+pip install librosa==0.7.0
+pip install ipdb
+pip install youtube_dl
+pip install spotipy
+pip install dominate
